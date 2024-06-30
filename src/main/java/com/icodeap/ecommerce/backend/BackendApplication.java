624b75b6bfb91bb7b2899d9f4b357f3e335ed94a
Hsm49 <jvazquezm1701@alumno.ipn.mx>
2024-06-30 14:35:48 -0600

    Added a command print with the used port, added port 8080 to app properties

--- src/main/java/com/icodeap/ecommerce/backend/BackendApplication.java
+++ src/main/java/com/icodeap/ecommerce/backend/BackendApplication.java
@@ -1,9 +1,11 @@
 package com.icodeap.ecommerce.backend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -11,5 +13,12 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+
+	@Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Server is running on port: " + ctx.getEnvironment().getProperty("server.port"));
+        };
+    }
 
 }
