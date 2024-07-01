1948a2dd4f552f06702de5ba60a81353332f539f
Hsm49 <jvazquezm1701@alumno.ipn.mx>
2024-07-01 15:16:12 -0600

    Added render URL to controllers

--- src/main/java/com/icodeap/ecommerce/backend/infrastructure/rest/CategoryController.java
+++ src/main/java/com/icodeap/ecommerce/backend/infrastructure/rest/CategoryController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/admin/categories")
 @Slf4j
-@CrossOrigin(origins = {"http://localhost:4200", "https://springstore.netlify.app/"})
+@CrossOrigin(origins = {"http://localhost:4200", "https://wb-backend-1.onrender.com"})
 public class CategoryController {
     private final CategoryService categoryService;
 
--- src/main/java/com/icodeap/ecommerce/backend/infrastructure/rest/OrderController.java
+++ src/main/java/com/icodeap/ecommerce/backend/infrastructure/rest/OrderController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/orders")
-@CrossOrigin(origins = {"http://localhost:4200", "https://springstore.netlify.app/"})
+@CrossOrigin(origins = {"http://localhost:4200", "https://wb-backend-1.onrender.com"})
 @Slf4j
 public class OrderController {
     private final OrderService orderService;
